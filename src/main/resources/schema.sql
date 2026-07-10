c4464689c4027ed9a9feba49a7dbd2c3e2bf722f
Alysson Aldrin <alysson_cdf@hotmail.com>
2026-07-10 00:26:08 -0300

    feat: configuração do banco Postgres via Docker Compose

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks
+(
+    id         BIGSERIAL PRIMARY KEY,
+    title      VARCHAR(255) NOT NULL,
+    completed  BOOLEAN      NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP    NOT NULL DEFAULT NOW()
+);
