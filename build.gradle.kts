efc61e1c80ed7ab8b96693800eaf6619fe68ca90
Alysson Aldrin <alysson_cdf@hotmail.com>
2026-07-12 15:01:38 -0300

    test: adiciona dependências do Testcontainers (Kafka)

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.4")
+    testImplementation("org.testcontainers:kafka:1.20.4")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.4")
+    testImplementation("org.testcontainers:kafka:1.20.4")
 }
 
 kotlin {
