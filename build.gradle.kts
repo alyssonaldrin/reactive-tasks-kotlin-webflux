2c7c7e5e0d3d602cda5627bf745817a6ed044b8d
Alysson Aldrin <alysson_cdf@hotmail.com>
2026-07-12 01:52:18 -0300

    feat: adiciona Actuator e Micrometer para expor métricas Prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("io.projectreactor.kafka:reactor-kafka:1.3.23")
     implementation("org.apache.kafka:kafka-clients:3.9.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
